77e4232d8721b7c6c0a0bbd3b260433bc058cae9
An Khanh <e0407674@u.nus.edu>
2021-11-15 14:35:38 +0800

    improve navbar and add workflows

--- management/src/components/BasicComponent/Navbar.vue
+++ management/src/components/BasicComponent/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
   <nav class="navbar navbar-expand-lg p-2 bg-dark text-white">
     <img style="width: 100px" src="@/assets/logo.png" alt="" />
-    <ul class="navbar-nav">
+    <ul class="navbar-nav p-2">
         <li class="nav-item m-2">
-        <a class="nav-link" href="./home">Home</a>
+        <router-link tag="li" class="nav-link" to="/home" >Home</router-link>
         </li>
         <li class="nav-item m-2">
-        <a class="nav-link" href="./chat">Chat</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="./whiteboard">Whiteboard</a>
+        <router-link tag="li" class="nav-link" to="/chat">Chat</router-link>
         </li>
         <li class="nav-item m-2">
-        <a class="nav-link" href="./profile">Profile</a>
+        <router-link tag="li" class="nav-link" to="/whiteboard">Whiteboard</router-link>
+        </li>
+        <li class="nav-item m-2">
+        <router-link tag="li" class="nav-link" to="/profile">Profile</router-link>
         </li>
         <li class="nav-item ml-6 m-auto">
             <LogOut/>
@@ -41,4 +41,13 @@ export default {
 </script>
 
 <style>
+nav li:active,
+nav li:hover,
+nav a.router-link-active,
+nav a.router-link-exact-active {
+   background-color: rgba(24, 100, 161, 0.774);
+   color:black;
+   cursor: pointer;
+ }
+
 </style>
